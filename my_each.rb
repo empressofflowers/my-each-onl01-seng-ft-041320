6b29b8fbc2899f380030d12f56ca38d0e9deefa0
Devone Austin <devone.austin@gmail.com>
2020-04-18 01:31:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,18 +4,17 @@ require 'pry'
 
 def my_each(array)
   if block_given?
+    n = 0
 
-  n = 0
+    while array < array.length
+      yield (array[n])
+      n = n + 1
+    end
 
-  while array < array.length
-    yield (array[n])
-    n = n + 1
-  end
-
-  array
+    array
   else
     return array
-
+  end
 end
 
 #my_each(array)
