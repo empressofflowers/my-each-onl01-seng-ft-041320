60e5fc8f8e6f4a445ee9e66fc771369f3553be61
Devone Austin <devone.austin@gmail.com>
2020-04-18 01:45:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,13 +3,14 @@ require 'pry'
 #fruit_basket = ["mango", "peach", "orange", "apple"]
 
 def my_each(array)
-  n = 0
-  while n < array.length
-    yield (array[n])
-    n = n + 1
-  end
+    if
+      n = 0
+      while n < array.length
+        yield (array[n])
+        n = n + 1
+      end
 
-  array = []
+      array
 end
 
 #my_each(array)
