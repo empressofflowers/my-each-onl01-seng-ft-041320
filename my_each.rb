5e5459ff73a909f697f8ef1b9a47a2154c3207aa
Devone Austin <devone.austin@gmail.com>
2020-04-18 01:30:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,6 +3,7 @@ require 'pry'
 #fruit_basket = ["mango", "peach", "orange", "apple"]
 
 def my_each(array)
+  if block_given?
 
   n = 0
 
@@ -12,7 +13,9 @@ def my_each(array)
   end
 
   array
-   
+  else
+    return array
+
 end
 
 #my_each(array)
